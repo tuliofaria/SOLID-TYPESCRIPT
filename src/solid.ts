5c3a2cbafd97b2e2f437795427c1ef2f89334bed
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-01 13:15:42 -0300

    c18- rewrite new code

--- src/solid.ts
+++ src/solid.ts
@@ -1,87 +1,38 @@
-// create interface first!!
-// connect about both type of class
+//Guidance about  OCP (-open-closed-Principle)
+interface Notafiscal{
+    valuebook:number;
+    namebook: string;
+}
 
-interface Book {
+//Guidance about SRP (SINGLE RESPONSIBILITY PRINCIPLE)
+class Libraries {
+    constructor(nomebook:string){
+     this.nomebook = nomebook
+    }
     nomebook: string;
-    ibm: number;
-    quantidade: number;
-    sumbook: number;
-    }
+}
+
+class Pricebook implements Notafiscal {
+    valuebook:number;
+    namebook: string;
     
-interface SALE {
-      pesquisa: string;
-      update: string;
+    constructor(pricebook:number){
+     this.pricebook = pricebook
     }
-    
-interface FinnalySale {
-    value:number ;
-    print: string;
-    }
-
-// create class uses guidance about SRP (SINGLE RESPONSIBILITY PRINCIPLE)
-class Libraries implements Book {  
-    nomebook: string;
-    ibm: number;
-    quantidade: number;
-    sumbook: number ;
-
-    constructor(nomebook:string, ibm: number, quantidade: number, sumbook: number){
-       this.nomebook = nomebook ,
-       this.ibm = ibm,
-       this.quantidade = quantidade,
-       this.sumbook = sumbook
-    }
+    pricebook: number;
 }
 
-class Organizesale implements SALE {
-    pesquisa: string ;
-    update: string ;
+class Finnaly implements Notafiscal {
+    valuebook:number;
+    namebook: string;
 
-    constructor(pesquisa: string, update: string){
-         this.pesquisa = pesquisa ,
-         this.update = update
+    constructor(bookone: number){
+        this.bookone = bookone 
     }
-}
-
-class Salefinal implements FinnalySale {
-    value:number ;
-    print: string;
-    
-    constructor(value:number, print: string){
-         this.value = this.value,
-         this.print = print
-    }
-}
-
-class ReceiveSale {
-    constructor(){
-        this
-    }     
+    bookone:number;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let caught: Libraries 
-caught = new Libraries('recipe about spice up meal',32321312 ,2,10);
-
-let survey: Organizesale
-survey = new Organizesale('Rearrange your brain','introducao a python');
-
-let together : Salefinal
-together = new Salefinal(100, 'show invoice')
+let fun1: Libraries = new Libraries('Learning Mysql')
+let prec: Pricebook = new Pricebook(99.90)
+let finasale: Finnaly = new Finnaly(99.90)
