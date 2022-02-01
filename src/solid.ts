e5a17751919c6f5630ecd3aad28900338392c552
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-01 09:26:20 -0300

    c6- create class and interface

--- src/solid.ts
+++ src/solid.ts
@@ -1,19 +1,66 @@
 // create class uses guidance about SRP (SINGLE RESPONSIBILITY PRINCIPLE)
 
-class Libraries {
-    constructor(){
-     return this
+interface Book {
+    nomebook: string;
+    ibm: number;
+    quantidade: number;
+    sumbook: number;
+    }
+    
+    interface SALE {
+      pesquisa: string;
+      update: string;
+    }
+    
+    interface Finnalysale {
+    value:number ;
+    print: string;
+    }
+
+// need create interface to together this clas
+///////////////////////////////////////////////////////////////////////
+
+class Libraries implements Book {  
+    nomebook: string;
+    ibm: number;
+    quantidade: number;
+    sumbook: number ;
+
+    constructor(nomebook:string, ibm: number, quantidade: number, sumbook: number){
+       this.nomebook = nomebook ,
+       this.ibm = ibm,
+       this.quantidade = quantidade,
+       this.sumbook = sumbook
     }
 }
 
-class Organizesale {
-    constructor(){
-        return this
+class Organizesale implements SALE {
+    pesquisa: string ;
+    update: string ;
+
+    constructor(pesquisa: string, update: string){
+         this.pesquisa = pesquisa ,
+         this.update = update
     }
 }
 
-class Salefinal {
-    constructor(){
-        return this
+class Salefinal implements Finnalysale {
+    value:number ;
+    print: string;
+    
+    constructor(value: number, print: string){
+         this.value = this.value,
+         this.print = print
     }
 }
+
+
+
+let caught: Libraries 
+caught = new Libraries('introducao a python',32321312 ,2,10);
+
+let survey: Organizesale
+survey = new Organizesale('introducao a python','introducao a python');
+
+let together : Salefinal
+together = new Salefinal(100, 'show invoice')
