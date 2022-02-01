01bda537da88eb0f97ac0257d05abf117d67fe23
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-01 15:03:42 -0300

    c24-adding new information

--- dist/solid.js
+++ dist/solid.js
@@ -1,3 +1,5 @@
+"use strict";
+exports.__esModule = true;
 //Guidance about SRP (SINGLE RESPONSIBILITY PRINCIPLE)
 var Libraries = /** @class */ (function () {
     function Libraries(nomebook) {
@@ -18,7 +20,6 @@ var Finnaly = /** @class */ (function () {
     return Finnaly;
 }());
 //get interface and adding values of book
-var Invoices = { valuebook: 99.90, namebook: 'learning Mysql' };
 var fun1 = new Libraries('Learning Mysql');
 var prec = new Pricebook(99.90);
 var finasale = new Finnaly(99.90);
