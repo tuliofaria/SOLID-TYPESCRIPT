df91760d8b5bac6ba887ec930f74975c9af246b9
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-01 15:06:02 -0300

    c28- try it more one time

---
+++ src/thought.ts
@@ -0,0 +1,10 @@
+
+
+//Guidance about  OCP (-open-closed-Principle)
+  interface Notafiscal{
+    valuebook:number;
+    namebook: string;
+}
+
+//get interface and adding values of book
+export {Notafiscal}
