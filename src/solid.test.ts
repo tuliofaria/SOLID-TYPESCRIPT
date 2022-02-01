905a17b0e556c1ce9618045503c8b372f90fd6c1
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-01 15:04:32 -0300

    c25- try it test unit

--- src/solid.test.ts
+++ src/solid.test.ts
@@ -1,5 +1,5 @@
 const Receive = require('./solid')
 
-test('test some command about typescript', () =>{
-  expect(Receive.Soma(42)).toBe(32)
+test('class' , () =>{
+  expect(Receive.Libraries(String)).toBe(String)
 })
