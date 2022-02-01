34e94132a052fe8bd53758e7ec4c2283a8892770
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-01 09:25:54 -0300

    c5- adding modify about type

--- dist/solid.js
+++ dist/solid.js
@@ -1,19 +1,32 @@
 // create class uses guidance about SRP (SINGLE RESPONSIBILITY PRINCIPLE)
+// need create interface to together this clas
+///////////////////////////////////////////////////////////////////////
 var Libraries = /** @class */ (function () {
-    function Libraries() {
-        return this;
+    function Libraries(nomebook, ibm, quantidade, sumbook) {
+        this.nomebook = nomebook,
+            this.ibm = ibm,
+            this.quantidade = quantidade,
+            this.sumbook = sumbook;
     }
     return Libraries;
 }());
 var Organizesale = /** @class */ (function () {
-    function Organizesale() {
-        return this;
+    function Organizesale(pesquisa, update) {
+        this.pesquisa = pesquisa,
+            this.update = update;
     }
     return Organizesale;
 }());
 var Salefinal = /** @class */ (function () {
-    function Salefinal() {
-        return this;
+    function Salefinal(value, print) {
+        this.value = this.value,
+            this.print = print;
     }
     return Salefinal;
 }());
+var caught;
+caught = new Libraries('introducao a python', 32321312, 2, 10);
+var survey;
+survey = new Organizesale('introducao a python', 'introducao a python');
+var together;
+together = new Salefinal(100, 'show invoice');
